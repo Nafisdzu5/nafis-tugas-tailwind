a79eb1fe140a79ce423e2f565402fb3618c90f31
Nafis Dzu <nafisdzu8@gmail.com>
2024-02-24 11:38:06 +0700

    Selesai Memuat Project

---
+++ pertemuan-1/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+        <>
+        <div className="flex mx-20 gap-10">
+            <div className="icon">
+                <img src="" alt="ini logo" />
+            </div>
+            <div className="nav flex gap-3 mr-64">
+                <div className="">Home</div>
+                <div className="">Studio</div>
+                <div className="">Works</div>
+                <div className="">Contact</div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Header;
